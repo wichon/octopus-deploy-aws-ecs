bed5d1707bd22a9c5d46791ef9836bf467b48e29
Luis Alonzo <wichon@gmail.com>
2016-01-22 17:16:12 -0600

    Create dockerize-artifacts.sh
    This will create a docker image from your artifacts by using a Docker file located in the root level of your artifacts

---
+++ dockerize-artifacts.sh
@@ -0,0 +1,20 @@
+dockerRepo=$(get_octopusvariable "DockerRepo")
+dockerBuildTag=$(get_octopusvariable "DockerBuildTag") 
+dockerRepoUser=$(get_octopusvariable "DockerRepoUser")
+dockerRepoPassword=$(get_octopusvariable "DockerRepoPassword")
+dockerRepoEmail=$(get_octopusvariable "DockerRepoEmail")
+
+# Cd into artifacts folder
+cd $(get_octopusvariable "Octopus.Action[Grab package].Output.Package.InstallationDirectoryPath")
+
+# Build image based in the docker file included in the root of the artifacts folder
+docker build -t $dockerRepo:$dockerBuildTag .
+
+# Tag generated container with the custom build tag, and the latest tag
+docker tag -f $dockerRepo:$dockerBuildTag $dockerRepo:latest 
+
+# Authenticate into Docker hub repository 
+docker login -u $dockerRepoUser -e $dockerRepoEmail -p $dockerRepoPassword
+
+# Push image to private repository with the latest and custom build tags
+docker push $dockerRepo:$dockerBuildTag && docker push $dockerRepo:latest
