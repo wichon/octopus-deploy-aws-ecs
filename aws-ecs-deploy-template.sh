bb44833c864c65f66675ff5cfc5455ad792e7609
Luis Alonzo <wichon@gmail.com>
2016-03-11 10:37:48 -0600

    Update aws-ecs-deploy-template.sh
    a little non functional refactor

--- aws-ecs-deploy-template.sh
+++ aws-ecs-deploy-template.sh
@@ -8,17 +8,18 @@ dockerUser=$(get_octopusvariable "DockerRepoUser")
 dockerPassword=$(get_octopusvariable "DockerRepoPassword")
 dockerRepo=$(get_octopusvariable "DockerRepo")
 dockerBuildTag=$(get_octopusvariable "DockerBuildTag")
+maxRetries=10
 
 i="0"
 echo "Checking if the Octopus Build image is registered in the Docker Hub repository ..."
-while [ $i -lt 10 ] && [ $(curl -s -m 2 --retry 3 --retry-delay 2 --retry-max-time 10 --user $dockerUser:$dockerPassword https://index.docker.io/v1/repositories/$dockerRepo/tags | jq ".[].name" | grep -c "$dockerBuildTag") -eq 0 ] ;
+while [ $i -lt $maxRetries ] && [ $(curl -s -m 2 --retry 3 --retry-delay 2 --retry-max-time 10 --user $dockerUser:$dockerPassword https://index.docker.io/v1/repositories/$dockerRepo/tags | jq ".[].name" | grep -c "$dockerBuildTag") -eq 0 ] ;
 do
     echo "Octopus Build image is not registered yet, sleeping 30 seconds and going to check back again (-_-)zzz"
     i=$[$i+1]
     sleep 30
 done
 
-if [ $i -eq 10 ] ; then
+if [ $i -eq $maxRetries ] ; then
     echo "Error!, Could not find the Octopus Build image ($dockerBuildTag) in the Docker Hub registry, deployment to AWS ECS will not continue :(."
     exit 1
 fi
