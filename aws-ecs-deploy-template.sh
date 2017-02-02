d95209e67f91ca385c3fffa81678c25c715651ed
wichon <wichon@gmail.com>
2017-02-02 16:22:01 -0600

    Added: latest changes to the deployment script

--- aws-ecs-deploy-template.sh
+++ aws-ecs-deploy-template.sh
@@ -1,6 +1,4 @@
 region=$(get_octopusvariable "Region")
-aws configure set region $region
-
 cluster=$(get_octopusvariable "Cluster")
 service=$(get_octopusvariable "Service")
 taskDefinition=$(get_octopusvariable "TaskDefinition") 
@@ -8,15 +6,16 @@ dockerUser=$(get_octopusvariable "DockerRepoUser")
 dockerPassword=$(get_octopusvariable "DockerRepoPassword")
 dockerRepo=$(get_octopusvariable "DockerRepo")
 dockerBuildTag=$(get_octopusvariable "DockerBuildTag")
-maxRetries=10
+maxRetries=15
+sleepPeriod=30
 
-i="0"
+i=0
 echo "Checking if the Octopus Build image is registered in the Docker Hub repository ..."
 while [ $i -lt $maxRetries ] && [ $(curl -s -m 2 --retry 3 --retry-delay 2 --retry-max-time 10 --user $dockerUser:$dockerPassword https://index.docker.io/v1/repositories/$dockerRepo/tags | jq ".[].name" | grep -c "$dockerBuildTag") -eq 0 ] ;
 do
-    echo "Octopus Build image is not registered yet, sleeping 30 seconds and going to check back again (-_-)zzz"
+    echo "Octopus Build image is not registered yet, sleeping $sleepPeriod seconds and going to check back again (-_-)zzz"
     i=$[$i+1]
-    sleep 30
+    sleep $sleepPeriod
 done
 
 if [ $i -eq $maxRetries ] ; then
@@ -24,16 +23,28 @@ if [ $i -eq $maxRetries ] ; then
     exit 1
 fi
 
-echo "Getting Active task defintion, and replacing the image name with the new one, to later create a new task definition revision ..."
-taskDefinitionBody=$(aws ecs describe-task-definition --task-definition $taskDefinition | jq '.taskDefinition.containerDefinitions' | jq ".[].image = \"$dockerRepo:$dockerBuildTag\"")
+echo "Getting Active task defintion ..."
+taskDefinitionBody=$(aws ecs describe-task-definition --region $region --task-definition $taskDefinition | jq '.taskDefinition.containerDefinitions' | jq ".[].image = \"$dockerRepo:$dockerBuildTag\"")
 
 echo "Registering new task definition revision containing the generated image ..."
-aws ecs register-task-definition --family $taskDefinition --container-definitions "$taskDefinitionBody"
+aws ecs register-task-definition --region $region --family $taskDefinition --container-definitions "$taskDefinitionBody"
 
 echo "Updating ECS service ..."
-aws ecs update-service --cluster $cluster --service $service --task-definition $taskDefinition
+aws ecs update-service --region $region --cluster $cluster --service $service --task-definition $taskDefinition
 
 sleep 10
-aws ecs wait services-stable --cluster $cluster --services $service
+aws ecs wait services-stable --region $region --cluster $cluster --services $service
+
+i=0
+serviceDetails=$(aws ecs describe-services --region $region --cluster $cluster --services "$service" | jq '{elbName: .services[0].loadBalancers[0].loadBalancerName, desiredCount: .services[0].desiredCount }')
+elbName=$(echo $serviceDetails | jq .elbName)
+desiredTasks=$(echo $serviceDetails | jq .desiredCount)
+echo "Checking that the load balancer has completely registered the new containers ..."
+while [ $i -lt $maxRetries ] && [ ! $(aws elb describe-instance-health --load-balancer-name $(echo $elbName | sed s/\"//g) | grep InService | wc -l) -eq $desiredTasks ]
+do
+    echo "The load balancer has not completely registered the new container versions, sleeping $sleepPeriod seconds and going to check back again (-_-)zzz"
+    i=$[$i+1]
+    sleep $sleepPeriod
+done
 
 echo "Application successfully deployed"
--- dockerize-artifacts.sh
+++ dockerize-artifacts.sh
@@ -10,11 +10,11 @@ cd $(get_octopusvariable "Octopus.Action[Grab package].Output.Package.Installati
 # Build image based in the docker file included in the root of the artifacts folder
 docker build -t $dockerRepo:$dockerBuildTag .
 
-# Tag generated container with the custom build tag, and the latest tag
+# Tag generated container with the custom tag, and the latest tag
 docker tag -f $dockerRepo:$dockerBuildTag $dockerRepo:latest 
 
 # Authenticate into Docker hub repository 
 docker login -u $dockerRepoUser -e $dockerRepoEmail -p $dockerRepoPassword
 
-# Push image to private repository with the latest and custom build tags
+# Push image to private repository with the latest and build number tags
 docker push $dockerRepo:$dockerBuildTag && docker push $dockerRepo:latest
